7120ec0c0bfd53b0a90db88a0b658e1feddbc407
TRINH TOAN TRUNG <trungtt@azgroup.co>
2021-03-03 14:09:10 +0700

    add jpa audit config

---
+++ src/main/java/com/nab/assignment/product/audit/AuditorAwareImpl.java
@@ -0,0 +1,13 @@
+package com.nab.assignment.product.audit;
+
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+public class AuditorAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+//        return Optional.empty();
+        return Optional.of("system");
+    }
+}
--- src/main/java/com/nab/assignment/product/model/AbstractAuditingEntity.java
+++ src/main/java/com/nab/assignment/product/model/AbstractAuditingEntity.java
@@ -3,16 +3,23 @@ package com.nab.assignment.product.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class AbstractAuditingEntity {
     @CreatedBy
     @Column(name = "created_by", nullable = false, length = 50, updatable = false)
